83a9c4faa5702de0daea834e74d4da71a19422f8
kcirekcom <erick.f.mock@gmail.com>
2017-01-26 22:36:33 -0800

    set up conts in webpack config

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+const HTMLPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
