c44738613319fbac55255c0b6ad3b5bc19f63f7b
kcirekcom <erick.f.mock@gmail.com>
2017-01-26 23:50:39 -0800

    authService set token and get token functionality created

---
+++ app/service/auth-service.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = ['$q', '$log', '$http', '$window', authService];
+
+function authService($q, $log, $http, $window) {
+  $log.debug('authService');
+
+  let service = {};
+  let token = null;
+
+  function setToken(_token) {
+    $log.debug('authService.setToken()');
+
+    if (! _token) {
+      return $q.reject(new Error('no token'));
+    }
+
+    $window.localStorage.setItem('token', _token);
+    token = _token;
+    return $q.resolve(token);
+  }
+
+  service.getToken = function() {
+    $log.debug('authService.getToken()');
+
+    if (token) {
+      return $q.resolve(token);
+    }
+
+    token = $window.localStorage.getItem('token');
+    if (token) return $q.resolve(token);
+    return $q.reject(new Error('token not found'));
+  };
+}
