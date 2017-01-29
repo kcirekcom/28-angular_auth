ea73766836fa490ed62d10916f5a26d502c25618
kcirekcom <erick.f.mock@gmail.com>
2017-01-29 14:28:23 -0800

    fetchGalleries method added into HomeController

--- app/view/home/home-controller.js
+++ app/view/home/home-controller.js
@@ -1,7 +1,22 @@
 'use strict';
 
-module.exports = ['$log', HomeController];
+module.exports = ['$log', '$rootScope', 'galleryService', HomeController];
 
-function HomeController($log) {
+function HomeController($log, $rootScope, galleryService) {
   $log.debug('HomeController');
+
+  this.galleries = [];
+
+  this.fetchGalleries = function() {
+    galleryService.fetchGalleries()
+    .then(galleries => {
+      this.galleries = galleries;
+    });
+  };
+
+  this.fetchGalleries();
+
+  $rootScope.$on('locationChangeSuccess', () => {
+    this.fetchGalleries();
+  });
 }
