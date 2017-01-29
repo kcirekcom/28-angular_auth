db3e6bc1b36e1c1894a03dfc99f036f39818f41a
kcirekcom <erick.f.mock@gmail.com>
2017-01-29 13:34:15 -0800

    createGallery service functionality added

---
+++ app/service/gallery-service.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = ['q', '$log', '$http', 'authService', galleryService];
+
+function galleryService($q, $log, $http, authService) {
+  $log.debug('galleryService');
+
+  let service = {};
+  service.galleries = [];
+
+  service.createGallery = function(gallery) {
+    $log.debug('galleryService.createGallery()');
+
+    return authService.getToken()
+    .then(token => {
+      let url = `${__API_URL__}/api/gallery`; // eslint-disable-line
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      };
+      return $http.post(url, gallery, config);
+    })
+    .then(res => {
+      $log.log('gallery created');
+
+      let gallery = res.data;
+      service.galleries.unshift(gallery);
+      return gallery;
+    })
+    .catch(err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  
+}
