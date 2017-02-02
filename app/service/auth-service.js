728c97df71537ee690fe680002c8b962fdd767ed
kcirekcom <erick.f.mock@gmail.com>
2017-02-02 11:56:35 -0800

    final styles added for lab 31

--- app/service/auth-service.js
+++ app/service/auth-service.js
@@ -42,7 +42,7 @@ function authService($q, $log, $http, $window){
   service.signup = function(user) {
     $log.debug('authService.signup()');
 
-    let url = `${__API_URL__}/api/signup`;
+    let url = `${__API_URL__}/api/signup`; //eslint-disable-line
     let config = {
       headers: {
         'Content-Type': 'application/json',
@@ -64,7 +64,7 @@ function authService($q, $log, $http, $window){
   service.login = function(user){
     $log.debug('authService.login()');
 
-    let url = `${__API_URL__}/api/login`;
+    let url = `${__API_URL__}/api/login`; //eslint-disable-line
     let base64 = $window.btoa(`${user.username}:${user.password}`);
     let config = {
       headers: {
